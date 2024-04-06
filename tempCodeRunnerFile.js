1c4fa96909967ca0fb700a530e490f3841344cea
Omar Faruk <omarfaruk65142@gmail.com>
2024-04-06 13:28:06 +0600

    2621 sleep  problem slove

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,14 @@
-counterFunction
+function sleep(millis) {
+    return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+// Test examples
+let t = Date.now();
+sleep(100).then(() => {
+    console.log(Date.now() - t); // Output: 100
+});
+
+t = Date.now();
+sleep(200).then(() => {
+    console.log(Date.now() - t); // Output: 200
+});
