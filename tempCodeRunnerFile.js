2bf4c33f1a6a726d2130fb7a3106be2c07c4f7ed
Omar Faruk <omarfaruk65142@gmail.com>
2024-04-06 11:16:18 +0600

    check if instance of problem

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,4 @@
-.reverse()
+ if (typeof value === 'object' && value !== null && typeof classOrSuperclass === 'function') {
+        const methods = Object.getOwnPropertyNames(classOrSuperclass.prototype);
+        return methods.every(method => typeof value[method] === 'function');
+    }
