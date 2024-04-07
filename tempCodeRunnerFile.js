1784a564b2700df8bf6678271b7d1d8642f2e024
Omar Faruk <omarfaruk65142@gmail.com>
2024-04-07 10:28:08 +0600

    2626 array reduce transformation

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,14 +1,1 @@
-function sleep(millis) {
-    return new Promise(resolve => setTimeout(resolve, millis));
-}
-
-// Test examples
-let t = Date.now();
-sleep(100).then(() => {
-    console.log(Date.now() - t); // Output: 100
-});
-
-t = Date.now();
-sleep(200).then(() => {
-    console.log(Date.now() - t); // Output: 200
-});
+array.push(newArray);
